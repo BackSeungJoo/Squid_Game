934c9679a674b5b0fa5387de4db45a7b94a5e656
BackSeungJoo <lueun59@gmail.com>
2023-06-26 13:09:21 +0900

    📰 Init: "오징어 게임" v0.0.0
    # 프로젝트 시작

---
+++ Squid_Game/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squid_Game
+{
+    internal class Program
+    {
+        public void Main(string[] args)
+        {
+        }
+    }
+}
